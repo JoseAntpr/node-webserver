393b11e3d1d20369dbd1a27a499639766012ce77
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-23 13:33:47 +0200

    ADD: serve static files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+/* Es una instruccion o callback que se ejecuta siempre.*/
+
+app.use( express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
