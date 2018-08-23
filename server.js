7be95b38744cf2cbfa1fe3cf1325df2782ec3645
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-23 14:23:03 +0200

    ADD: work with Handlebars

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 
 /* Es una instruccion o callback que se ejecuta siempre.*/
-
 app.use( express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
+// Express HBS engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Jose Antonio',
+        anio: new Date().getFullYear()
+    });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
