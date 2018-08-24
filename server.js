1df8e6b4bacd485661d7f034e44469bf5be0dd31
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-24 08:42:30 +0200

    ADD: add partials for build templates & create some helpers

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
+// Helpers
+require('./hbs/helpers');
 
 /* Es una instruccion o callback que se ejecuta siempre.*/
 app.use( express.static(__dirname + '/public'));
 
 // Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
 
 app.get('/', (req, res) => {
     res.render('home', {
-        nombre: 'Jose Antonio',
-        anio: new Date().getFullYear()
+        nombre: 'jose antonio'
     });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => {
